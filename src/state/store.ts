a62ed067e2e3080817ba46cc9252d6017e92b593
MKotliarenko <kotliarenkom282@gmail.com>
2023-01-04 21:07:14 +0100

    fix:ghPages

---
+++ src/state/store.ts
@@ -0,0 +1,27 @@
+import {tasksReducer} from './tasks-reducer';
+import {todolistsReducer} from './todolists-reducer';
+import {combineReducers, createStore} from 'redux';
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+
+// объединяя reducer-ы с помощью combineReducers,
+// мы задаём структуру нашего единственного объекта-состояния
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
+// непосредственно создаём store
+export const store = createStore(rootReducer);
+// определить автоматически тип всего объекта состояния
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+// @ts-ignore
+window.store = store;
